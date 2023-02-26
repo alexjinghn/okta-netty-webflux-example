e8579f0a78c6d8231f3c944e42e7cfafe6890c50
Alex Jing <alex.jing@databricks.com>
2023-02-26 15:31:19 -0800

    add script

--- netty-hello-world/run.sh
+++ netty-hello-world/run.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 ./gradlew build
-#java -cp ./build/libs/netty-hello-world.jar com.test.netty.server.AppServer &> /databricks/server_stdout &
-#java -cp ./build/libs/netty-hello-world.jar com.test.netty.client.Client &> /databricks/client_stdout
-
-java -cp ./build/libs/netty-hello-world.jar com.test.netty.server.AppServer &
-java -cp ./build/libs/netty-hello-world.jar com.test.netty.client.Client
+java -cp ./build/libs/netty-hello-world.jar com.test.netty.server.AppServer &> /databricks/server_stdout &
+java -cp ./build/libs/netty-hello-world.jar com.test.netty.client.Client &> /databricks/client_stdout
