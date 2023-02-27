342a3ceea4a28b259cef6889d189dfe1ab9e3219
Alex Jing <alex.jing@databricks.com>
2023-02-26 16:22:13 -0800

    sleep between server and client

--- netty-hello-world/run.sh
+++ netty-hello-world/run.sh
@@ -2,4 +2,5 @@
 
 ./gradlew build
 java -cp ./build/libs/netty-hello-world.jar com.test.netty.server.AppServer &> /databricks/server_stdout &
+sleep 3
 java -cp ./build/libs/netty-hello-world.jar com.test.netty.client.Client &> /databricks/client_stdout
